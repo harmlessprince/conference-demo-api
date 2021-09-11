0b57fc558c9f69b81deb1ec1f2d1d9175c566267
harmlessprince <realolamilekan@gmail>
2021-09-11 19:08:40 +0100

    first commit

---
+++ src/main/java/com/teeque/conferencedemo/controllers/SpeakersController.java
@@ -0,0 +1,47 @@
+package com.teeque.conferencedemo.controllers;
+
+import com.teeque.conferencedemo.models.Speaker;
+import com.teeque.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list (){
+        System.out.println(speakerRepository.findAll());
+        return speakerRepository.findAll();
+    }
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    //    @ResponseStatus(HttpStatus.DE)
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PATCH)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker,"speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+}
